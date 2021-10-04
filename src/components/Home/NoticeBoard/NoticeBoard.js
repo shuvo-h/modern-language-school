51932831299e54a539edc05b5e58949abbef9b46
shuvo-h <shuvowriter@gmail.com>
2021-10-04 19:38:06 +0600

    margin notice board

--- src/components/Home/NoticeBoard/NoticeBoard.js
+++ src/components/Home/NoticeBoard/NoticeBoard.js
@@ -6,7 +6,7 @@ import { FcNews } from "react-icons/fc";
 const NoticeBoard = () => {
     return (
         <div className="col-md-4 ps-5 shadow">
-            <h2 className="text-center text-info"><u>Notice Board</u></h2>
+            <h2 className="text-center text-info my-4"><u>Notice Board</u></h2>
             <p><Link className="text-decoration-none" to="/cultural-ceremony"><FcNews/> Cultural ceremony -2021</Link></p>
             <p><Link className="text-decoration-none" to="/duration-reduce"><FcNews/> Course duration reduce due to Covid-19</Link></p>
             <p><Link className="text-decoration-none" to="/board-meeting"><FcNews/> Board meeting notice</Link></p>
